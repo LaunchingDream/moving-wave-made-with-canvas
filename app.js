7e392a0c4c3b2ff8f786fc9658f218096ba7e8aa
Seunghyun Yu <yshyun123@gmail.com>
2020-05-11 23:51:14 +0900

    Implement point and wave

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import Wave from "./wave";
+
 class App {
     constructor() {
         console.log('constructor()');
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
+        this.wave = new Wave();
 
         // Resize 이벤트 걸어주기
         window.addEventListener('resize', this.resize.bind(this), false);
@@ -21,11 +25,12 @@ class App {
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
+
+        this.wave.resize(this.stageWidth, this.stageHeight);
     }
 
     animate() {
-        // 캔버스 클리어
-        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+        this.wave.draw(this.ctx);
         requestAnimationFrame(this.animate.bind(this));
     }
 }
