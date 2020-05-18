dbd7ec8546d3eb1f1accccb84e31b4d7eeebb1e3
Seunghyun Yu <yshyun123@gmail.com>
2020-05-18 23:46:26 +0900

    Fix error

--- wave.js
+++ wave.js
@@ -1,55 +1,56 @@
-import Point from './point.js';
+import Point from "./point.js";
 
 export default class Wave {
-    constructor(index, totalPoints, color){
+    constructor(index,totalPoints,color){
         this.index = index;
         this.totalPoints = totalPoints;
         this.color = color;
         this.points = [];
+
     }
 
-    resize(stageWidth, stageHeight) {
+    resize(stageWidth, stageHeight){
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
 
-        this.centerX = stageWidth / 2;
-        this.centerY = stageHeight / 2;
+        this.centerX = stageWidth/2;
+        this.centerY = stageHeight/2;
 
-        this.pointGap = this.stageWidth / (this.totalPoints-1);
+        this.pointGap = this.stageWidth/(this.totalPoints-1);
 
         this.init();
     }
 
-    init() {
+    init(){
         this.points = [];
-        for (let i = 0 ; this.totalPoints; i++) {
+
+        for(let i = 0; i<this.totalPoints;i++){
             const point = new Point(
                 this.index + i,
                 this.pointGap * i,
-                this.centerY
+                this.centerY,
             );
             this.points[i] = point;
         }
     }
-
-    draw(ctx) {
+    draw(ctx){
         ctx.beginPath();
         ctx.fillStyle = this.color;
 
         let prevX = this.points[0].x;
         let prevY = this.points[0].y;
 
-        ctx.moveTo(prevX, prevY);
-
-        for(let i = 1 ; i < this.totalPoints ; i++){ß
-            if (i < this.totalPoints - 1){
+        ctx.moveTo(prevX,prevY);
+        
+        for(let i=1;i<this.totalPoints;i++){
+            if(i<this.totalPoints-1){
                 this.points[i].update();
             }
 
-            const cx = (prevX + this.points[i].x) / 2;
-            const cy = (prevY + this.points[i].y) / 2;
+            const cx = (prevX + this.points[i].x)/2;
+            const cy = (prevY + this.points[i].y)/2;
 
-            ctx.quadraticCurveTo(prevX, prevY, cx, cy);
+            ctx.quadraticCurveTo(prevX,prevY,cx,cy);
 
             prevX = this.points[i].x;
             prevY = this.points[i].y;
@@ -57,9 +58,10 @@ export default class Wave {
         }
 
         ctx.lineTo(prevX, prevY);
-        ctx.lineTo(this.stageWidth, this.stageHeight);
-        ctx.lineTo(this.points[0].x, this.stageHeightß);
+        ctx.lineTo(this.stageWidth,this.stageHeight);
+        ctx.lineTo(this.points[0].x,this.stageHeight);
         ctx.fill();
         ctx.closePath();
+        
     }
 }
