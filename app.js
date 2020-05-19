b137e57716ca462c697242e5563bd00a6784438f
Seunghyun Yu <yshyun123@gmail.com>
2020-05-19 23:57:44 +0900

    Adjust indents and spacing

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-import WaveGroup from './wavegroup.js';
+import WaveGroup from "./wavegroup.js";
 
 class App {
-    constructor() {
-        this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
-        document.body.appendChild(this.canvas);
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
 
-        this.waveGroup = new WaveGroup();
+    this.waveGroup = new WaveGroup();
 
-        // Resize 이벤트 걸어주기
-        window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
+    // Resize 이벤트 걸어주기
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
 
-        // 애니메이션 시작
-        requestAnimationFrame(this.animate.bind(this));
-    }
+    // 애니메이션 시작
+    requestAnimationFrame(this.animate.bind(this));
+  }
 
-    resize() {
-        this.stageWidth = document.body.clientWidth;
-        this.stageHeight = document.body.clientHeight;
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
 
-        // 레티나 디스플레이에서도 잘 보일 수 있도록 설정
-        this.canvas.width = this.stageWidth * 2;
-        this.canvas.height = this.stageHeight * 2;
-        this.ctx.scale(2, 2);
+    // 레티나 디스플레이에서도 잘 보일 수 있도록 설정
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
 
-        this.waveGroup.resize(this.stageWidth, this.stageHeight);
-    }
+    this.waveGroup.resize(this.stageWidth, this.stageHeight);
+  }
 
-    animate(t) {
-        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-        this.waveGroup.draw(this.ctx);
-        requestAnimationFrame(this.animate.bind(this));
-    }
+  animate(t) {
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    this.waveGroup.draw(this.ctx);
+    requestAnimationFrame(this.animate.bind(this));
+  }
 }
 
 window.onload = () => {
-    new App();
-}
+  new App();
+};
